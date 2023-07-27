8e94cb38fe436f216c949d5f7cfe3006dea405fa
kou12345 <hi.0412.ko.2002@gmail.com>
2023-07-27 11:02:12 +0900

    完成

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/kou12345/ent-bbs/ent"
+	"github.com/kou12345/ent-bbs/ent/entry"
+	"github.com/labstack/echo"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	client, err := ent.Open("sqlite3", "file:bbs.sqlite?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		eq := client.Entry.Query().Order(ent.Desc(entry.FieldCreatedAt)).Limit(10)
+		entries := eq.AllX(context.Background())
+
+		// entriesをJSON形式に変換して返す
+		return c.JSON(http.StatusOK, entries)
+	})
+	e.POST("/add", func(c echo.Context) error {
+		e := client.Entry.Create()
+		e.SetContent(c.FormValue("content"))
+		if _, err := e.Save(context.Background()); err != nil {
+			log.Println(err.Error())
+			return c.String(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		}
+		return c.Redirect(http.StatusFound, "/")
+	})
+	e.Logger.Fatal(e.Start(":8989"))
+}
