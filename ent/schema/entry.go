720ebbb7afa811663c4947ade6a911a43528a84d
kou12345 <hi.0412.ko.2002@gmail.com>
2023-07-27 10:21:03 +0900

    go generate ./ent

---
+++ ent/schema/entry.go
@@ -0,0 +1,28 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Entry holds the schema definition for the Entry entity.
+type Entry struct {
+	ent.Schema
+}
+
+// Fields of the Entry.
+func (Entry) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("content").Default(""),
+		field.Time("created_at").Default(func() time.Time {
+			return time.Now()
+		}),
+	}
+}
+
+// Edges of the Entry.
+func (Entry) Edges() []ent.Edge {
+	return nil
+}
